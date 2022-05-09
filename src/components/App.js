3e09e1d2315534965621189fa67bd7fa6e593d6b
Sahil Khadka <sahilkhadka767@gmail.com>
2022-05-09 09:50:43 +0545

    bug fixes

--- src/components/App.js
+++ src/components/App.js
@@ -3,72 +3,72 @@ import ToDOList from "./ToDoList"
 import InputText from "./InputText"
 
 function App() {
-  const [deleteBtn, setDeleteBtn] = useState(false)
-  const [listItemFinal, setListItemFinal] = useState([])
-  function handleButton(listItem) {
-    setListItemFinal((prevState) => {
-      if (!listItem.trim().length)
-        return [...prevState]
+	const [deleteBtn, setDeleteBtn] = useState(false)
+	const [listItemFinal, setListItemFinal] = useState([])
+	function handleButton(listItem) {
+		setListItemFinal((prevState) => {
+			if (!listItem.trim().length)
+				return [...prevState]
 
-      else {
-        setDeleteBtn(true)
-        return [...prevState, listItem]
-      }
-    })
-  }
-  function deleteItem(list) {
-    setListItemFinal(prevState => {
-      return prevState.filter(
-        (item) => {
-          return item !== list
-        }
-        )
-      })
-    }
-    useEffect(() => {
-      const items = JSON.parse(localStorage.getItem("lists"))
-      if (items.length) {
-        setListItemFinal(items)
-        setDeleteBtn(true)
-      }
-    }, [])
-    useEffect(() => {
-      localStorage.setItem("lists", JSON.stringify(listItemFinal))
-    }, [listItemFinal])
-    return (
-      <div className="container">
-        <div className="heading">
-            <h1>To-Do List</h1>
-        </div>
-        <InputText
-            onButtonClick = {handleButton}
-            />    
-        <div>
-            <ul>
-            {listItemFinal.map((item, index) => {
-              return (
-                <ToDOList 
-                key = {index}
-                listItem = {item} 
-                onChecked = {deleteItem}
-                />
-                )
-            }
-            )}
-            </ul>
-        </div>
-        <button 
-            type="button"
-            style={{display: deleteBtn ? "block" : "none"}}
-            onDoubleClick ={ () => {
-                setListItemFinal([])
-                setDeleteBtn(false)
-            } }
-            className="deleteBtn">
-            Delete All
-        </button>
-    </div>
-  )
+			else {
+				setDeleteBtn(true)
+				return [...prevState, listItem]
+			}
+		})
+	}
+	function deleteItem(list) {
+		setListItemFinal(prevState => {
+			return prevState.filter(
+				(item) => {
+					return item !== list
+				}
+			)
+		})
+	}
+	useEffect(() => {
+		const items = JSON.parse(localStorage.getItem("lists"))
+		if (items) {
+			setListItemFinal(items)
+			setDeleteBtn(true)
+		}
+	}, [])
+	useEffect(() => {
+		localStorage.setItem("lists", JSON.stringify(listItemFinal))
+	}, [listItemFinal])
+	return (
+		<div className="container">
+			<div className="heading">
+				<h1>To-Do List</h1>
+			</div>
+			<InputText
+				onButtonClick={handleButton}
+			/>
+			<div>
+				<ul>
+					{listItemFinal.map((item, index) => {
+						return (
+							<ToDOList
+								key={index}
+								listItem={item}
+								onChecked={deleteItem}
+							/>
+						)
+					}
+					)}
+				</ul>
+			</div>
+			<button
+				type="button"
+				style={{ display: deleteBtn ? "block" : "none" }}
+				onDoubleClick={() => {
+					setListItemFinal([])
+					setDeleteBtn(false)
+				}}
+				className="deleteBtn">
+				Delete All
+			</button>
+		</div>
+	)
 }
 
 export default App
--- src/components/InputText.js
+++ src/components/InputText.js
@@ -6,14 +6,21 @@ function InputText(props) {
     const [listItem, setListItem] = useState("")
     function handleInput(event) {
         setListItem(event.target.value)
+      }
+    const handleEnter = (event) => {
+      if(event.code === "Enter") {
+        setInput()
+      }
+    }
+    const setInput = () => {
+      props.onButtonClick(listItem)
+      setListItem("")
     }
     return (
         <div className="form">
-        <input type="text" onChange={handleInput} value={listItem} />
-        <button onClick={() => {
-            props.onButtonClick(listItem)
-            setListItem("")
-        }}>
+        <input type="text" onChange={handleInput} value={listItem} onKeyDown = {handleEnter}/>
+        <button onClick={setInput}
+        >
           <span>
             <AddIcon />
           </span>
